ede5337fe063efd8c7df7450a83aff7b82531dc7
Robert Ochoa <ochoa.robert1@gmail.com>
2021-03-20 15:19:10 -0400

    Metaboxes para Incidencias creados

--- inc/class-custom-metaboxes.php
+++ inc/class-custom-metaboxes.php
@@ -22,13 +22,161 @@ if (!class_exists('Sigsec_Metaboxes_Class')) :
          */
         public function __construct()
         {
-            add_action('cmb2_admin_init', array($this, 'register_custom_metabox'));
+            add_action('cmb2_admin_init', array($this, 'register_custom_metabox_taxonomy'));
+            add_action('cmb2_admin_init', array($this, 'register_custom_metabox_incidencias'));
         }
 
         /**
-         * REMOVE / HIDE MENU ON USERS
+         * CUSTOM METABOXES FOR INCIDENCIAS
          */
-        public function register_custom_metabox()
+        public function register_custom_metabox_incidencias()
+        {
+            $cmb_incidencias_metabox = new_cmb2_box(array(
+                'id'            => self::PREFIX . 'incidencias_metabox',
+                'title'         => esc_html__('Información Principal', parent::PLUGIN_SLUG),
+                'object_types'  => array('incidencias'),
+                'context'       => 'normal',
+                'priority'      => 'high',
+                'classes'       => parent::PLUGIN_SLUG . '-cmb2-wrapper',
+                'cmb_styles'    => false
+            ));
+
+            $cmb_incidencias_metabox->add_field(array(
+                'name'       => esc_html__('Hora de Inicio', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Seleccione la Hora de Inicio', parent::PLUGIN_SLUG),
+                'id'         => self::PREFIX . 'start_time',
+                'type' => 'text_time',
+                'attributes' => array(
+                    'data-timepicker' => json_encode(array(
+                        'timeOnlyTitle' => __('Escoger Hora y Minutos', parent::PLUGIN_SLUG),
+                        'timeFormat' => 'HH:mm',
+                        'stepMinute' => 1
+                    )),
+                ),
+                'time_format' => 'h:i A'
+            ));
+
+            $cmb_incidencias_metabox->add_field(array(
+                'id'         => self::PREFIX . 'turno',
+                'name'       => esc_html__('Turno / Guardia', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Escriba quienes estaban de guardia en el momento de la incidencia', parent::PLUGIN_SLUG),
+                'type'    => 'text_medium'
+            ));
+
+            
+
+            $cmb_incidencias_metabox->add_field(array(
+                'id'         => self::PREFIX . 'observaciones',
+                'name'       => esc_html__('Observaciones', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Ingrese una descripción de la incidencia', parent::PLUGIN_SLUG),
+                'type'    => 'wysiwyg',
+                'options' => array(
+                    'wpautop' => true,
+                    'media_buttons' => false,
+                    'textarea_rows' => get_option('default_post_edit_rows', 3),
+                    'teeny' => true
+                ),
+            ));
+
+            $cmb_incidencias_galerias_metabox = new_cmb2_box(array(
+                'id'            => self::PREFIX . 'incidencias_gallery_metabox',
+                'title'         => esc_html__('Información de Galería', parent::PLUGIN_SLUG),
+                'object_types'  => array('incidencias'),
+                'context'       => 'normal',
+                'priority'      => 'high',
+                'classes'       => parent::PLUGIN_SLUG . '-cmb2-wrapper',
+                'cmb_styles'    => false
+            ));
+
+            $cmb_incidencias_galerias_metabox->add_field(array(
+                'id'         => self::PREFIX . 'gallery',
+                'name'       => esc_html__('(OPCIONAL) Galería de Imágenes', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Cargue o Seleccione un listado de imagenes correspondiente a esta incidencia', parent::PLUGIN_SLUG),
+                'type'    => 'file_list',
+                'preview_size' => array(100, 100),
+                'query_args' => array('type' => 'image'),
+                'text' => array(
+                    'add_upload_files_text' => __('Agregar Imágenes', parent::PLUGIN_SLUG),
+                    'remove_image_text' => __('Remover Imagen', parent::PLUGIN_SLUG),
+                    'file_text' => __('Imagen', parent::PLUGIN_SLUG),
+                    'file_download_text' => __('Descargar', parent::PLUGIN_SLUG),
+                    'remove_text' => __('Remover', parent::PLUGIN_SLUG)
+                )
+            ));
+
+
+            $cmb_incidencias_vehiculo_metabox = new_cmb2_box(array(
+                'id'            => self::PREFIX . 'incidencias_vehiculo_metabox',
+                'title'         => esc_html__('Información de Transporte', parent::PLUGIN_SLUG),
+                'object_types'  => array('incidencias'),
+                'context'       => 'normal',
+                'priority'      => 'high',
+                'classes'       => parent::PLUGIN_SLUG . '-cmb2-wrapper',
+                'cmb_styles'    => false
+            ));
+
+            $cmb_incidencias_vehiculo_metabox->add_field(array(
+                'id'         => self::PREFIX . 'check_vehiculo',
+                'name'       => esc_html__('¿Esta implicado un vehiculo?', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Activar solo si en la incidencia esta involucrado un vehiculo', parent::PLUGIN_SLUG),
+                'type'    => 'checkbox'
+            ));
+
+            $arr_vehiculos = array();
+            $arr_query_vehiculos = new WP_Query(array('post_type' => 'vehiculos', 'posts_per_page' => -1));
+            if ($arr_query_vehiculos->have_posts()) :
+                while ($arr_query_vehiculos->have_posts()) : $arr_query_vehiculos->the_post();
+                    $arr_vehiculos[get_the_ID()] = get_the_title();
+                endwhile;
+            endif;
+            wp_reset_query();
+
+            $cmb_incidencias_vehiculo_metabox->add_field(array(
+                'name'    => 'Vehiculo(s) implicados',
+                'id'      => self::PREFIX . 'vehiculos',
+                'desc'    => 'Seleccione el/los vehiculos relacionados con la incidencia.',
+                'type'    => 'pw_multiselect',
+                'options' => $arr_vehiculos
+            ));
+
+
+            $cmb_incidencias_close_metabox = new_cmb2_box(array(
+                'id'            => self::PREFIX . 'incidencias_close_metabox',
+                'title'         => esc_html__('Cierre de Incidencia', parent::PLUGIN_SLUG),
+                'object_types'  => array('incidencias'),
+                'context'       => 'side',
+                'priority'      => 'high',
+                'classes'       => parent::PLUGIN_SLUG . '-cmb2-wrapper ' . parent::PLUGIN_SLUG . '-cmb2-close-incidencia',
+                'cmb_styles'    => false
+            ));
+
+            $cmb_incidencias_close_metabox->add_field(array(
+                'id'         => self::PREFIX . 'check_cierre',
+                'name'       => esc_html__('¿Esta incidencia esta cerrada?', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Activar solo si esta incidencia esta cerrada / finalizada', parent::PLUGIN_SLUG),
+                'type'    => 'checkbox'
+            ));
+
+            $cmb_incidencias_close_metabox->add_field(array(
+                'name'       => esc_html__('Hora de Cierre', parent::PLUGIN_SLUG),
+                'desc'       => esc_html__('Seleccione la Hora de Cierre', parent::PLUGIN_SLUG),
+                'id'         => self::PREFIX . 'end_time',
+                'type' => 'text_time',
+                'attributes' => array(
+                    'data-timepicker' => json_encode(array(
+                        'timeOnlyTitle' => __('Escoger Hora y Minutos', parent::PLUGIN_SLUG),
+                        'timeFormat' => 'HH:mm',
+                        'stepMinute' => 1
+                    )),
+                ),
+                'time_format' => 'h:i A'
+            ));
+        }
+
+        /**
+         * CUSTOM METABOXES FOR TAXONOMY
+         */
+        public function register_custom_metabox_taxonomy()
         {
             $cmb_term_metabox = new_cmb2_box(array(
                 'id'            => self::PREFIX . 'term_metabox',
